e33ce7c6da0a2e0a0cee4614c5d1c2ea3ed64863
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-04-21 19:04:38 +0300

    Added main styles, scripts with components and etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import { VideoCardComponent } from './video-card/video-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/video-card/video-card.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-video-card',
+  templateUrl: './video-card.component.html',
+  styleUrls: ['./video-card.component.scss']
+})
+export class VideoCardComponent implements OnInit {
+
+  constructor() {}
+
+  number = [5, 3, 2, 1];
+
+  ngOnInit(): void {
+  }
+
+}
