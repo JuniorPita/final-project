b4c900533796b30899ab2c23dfed39cf47a63d1b
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-04-22 20:11:01 +0300

    Added favicon, new components, new styles and HTML code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import { VideoCardComponent } from './video-card/video-card.component';
+import { VideoPageComponent } from './video-page/video-page.component';
+import { HeaderComponent } from './header/header.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoCardComponent
+    VideoCardComponent,
+    VideoPageComponent,
+    HeaderComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/video-page/video-page.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-video-page',
+  templateUrl: './video-page.component.html',
+  styleUrls: ['./video-page.component.scss']
+})
+export class VideoPageComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
