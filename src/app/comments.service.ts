e0adcd88efef23c47e6231afb8b45a67ab8efce7
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-05-03 22:11:06 +0300

    Comments Service was edited

--- src/app/comments.service.ts
+++ src/app/comments.service.ts
@@ -22,7 +22,7 @@ export class CommentsService {
   }
 
   public init(): void {
-    this.comments = JSON.parse(localStorage.getItem('comms') || '') || [];
+    this.comments = JSON.parse(localStorage.getItem('comms')!) || [];
     this._commentList$.next(this.comments);
   }
 }
